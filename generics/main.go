8130e548224763f736ba482bb518dd59a5d480f1
v.balabanov <v.balabanov85@gmail.com>
2025-05-07 21:46:53 +0300

    mes

--- generics/main.go
+++ generics/main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+// закладка: https://go.dev/doc/tutorial/generics#declare_type_constraint
+
 func main() {
 	ints := map[string]int64{
 		"first":  34,
@@ -16,6 +18,24 @@ func main() {
 	fmt.Printf("Non-Generic Sums: %v and %v\n",
 		SumInts(ints),
 		SumFloats(floats))
+
+	fmt.Printf("Generic Sums: %v and %v\n",
+		SumIntsOrFloats[string, int64](ints),
+		SumIntsOrFloats[string, float64](floats))
+
+	fmt.Printf("Generic Sums, type parameters inferred: %v and %v\n",
+		SumIntsOrFloats(ints),
+		SumIntsOrFloats(floats))
+}
+
+// SumIntsOrFloats sums the values of map m. It supports both int64 and float64
+// as types for map values.
+func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+	return s
 }
 
 // Sums intsin the map

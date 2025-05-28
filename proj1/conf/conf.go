8d54667663a32a693de005cff413cad0a21cd1af
dec1i1bel <v.balabanov85@gmail.com>
2025-05-28 19:22:48 +0300

    gitignore

--- proj1/conf/conf.go
+++ proj1/conf/conf.go
@@ -4,7 +4,7 @@ import "os"
 
 func SetDbAccess() {
 	os.Setenv("USER", "root")
-	os.Setenv("PASSWD", "vkshmuk0707")
+	os.Setenv("PASSWD", "dev")
 	os.Setenv("NET_TYPE", "tcp")
 	os.Setenv("HOST_PORT", "127.0.0.1:3306")
 	os.Setenv("DB_NAME", "proj1")

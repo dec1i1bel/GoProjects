47135c45d3303362509588cd537359691f10ef8e
dec1i1bel <v.balabanov85@gmail.com>
2025-06-04 19:37:32 +0300

    ..

--- proj1/app/app.go
+++ proj1/app/app.go
@@ -3,10 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
 	"proj1/conf"
-	"proj1/db"
 	"proj1/db/product"
+	"strconv"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,35 +14,58 @@ import (
 func main() {
 	conf.SetDbAccess()
 
-	/* toDo:
-	+ получить тестовые товары
-	+ вывести их в api
-		+ перенести получение в метод, подключить его к роутеру
-		+ в db\product корректно сформировать объект для json
-		+ в app сконвертировать struct в json
-	- получить на фронт в js
+	/* toDo - api:
+	> роуты:
+		+ все товары
+		+ товар по id
+		> POST добавление товара
+			https://go.dev/doc/tutorial/web-service-gin#add_item
+		- GET удаление товара
+	- добавление пользователя по email и паролю
+	- авторизация
+		- отправка кода подтверждения на email
+		- проверка кода
+	- ограничение доступности роутов для неавторизованного
+		- неавторизованному - получение максимум трёх товаров
+		- авторизованному - все роуты без ограничений
+	- у авторизованного - временная сессия по токену. по истечении срока - запрос повторной авторизации по api
 	*/
 
 	router := gin.Default()
 	router.GET("/products", findProducts)
+	router.GET("/product/:id", findProductById)
+	// router.POST("/product/add", addProduct)
 
 	router.Run("localhost:8080")
 }
 
-func findProducts(context *gin.Context) {
-	dbCon := db.Connect(
-		os.Getenv("USER"),
-		os.Getenv("PASSWD"),
-		os.Getenv("NET_TYPE"),
-		os.Getenv("HOST_PORT"),
-		os.Getenv("DB_NAME"),
-	)
-
-	products, err := product.FindAll(dbCon)
+func findProducts(c *gin.Context) {
+	products, err := product.FindAll()
 
 	if err != nil {
 		fmt.Printf("error getting products: %v", err)
 	}
 
-	context.JSON(http.StatusOK, products)
+	c.JSON(http.StatusOK, products)
+}
+
+func findProductById(c *gin.Context) {
+	idRaw := strings.TrimSpace(c.Param("id"))
+	id, err := strconv.ParseInt(idRaw, 10, 64)
+
+	if err != nil {
+		fmt.Printf("error converting param id <%v> to int\n", idRaw)
+	}
+
+	product, err := product.FindById(id)
+
+	if err != nil {
+		fmt.Printf("error getting product by id %d\n", id)
+	}
+
+	c.JSON(http.StatusOK, product)
+}
+
+func addProduct(c *gin.Context) (int64, error) {
+	var product Product
 }
--- proj1/db/product/product.go
+++ proj1/db/product/product.go
@@ -3,6 +3,8 @@ package product
 import (
 	"database/sql"
 	"fmt"
+	"os"
+	"proj1/db"
 )
 
 type Product struct {
@@ -16,9 +18,10 @@ type Product struct {
 	Weight_gr   float32
 }
 
-func FindAll(dbCon *sql.DB) ([]Product, error) {
+// func FindAll(dbCon *sql.DB) ([]Product, error) {
+func FindAll() ([]Product, error) {
 	var products []Product
-
+	dbCon := CreateDbConnection()
 	rows, err := dbCon.Query("SELECT id, active, name, description, xml_id, width_mm, height_mm, weight_gr FROM product")
 
 	if err != nil {
@@ -38,4 +41,30 @@ func FindAll(dbCon *sql.DB) ([]Product, error) {
 	}
 
 	return products, nil
+}
+
+func FindById(id int64) (Product, error) {
+	var product Product
+	dbCon := CreateDbConnection()
+	row := dbCon.QueryRow("SELECT id, active, name, description, xml_id, width_mm, height_mm, weight_gr FROM product WHERE id = ?", id)
+
+	if err := row.Scan(&product.Id, &product.Active, &product.Name, &product.Description, &product.Xml_id, &product.Width_mm, &product.Height_mm, &product.Weight_gr); err != nil {
+		if err == sql.ErrNoRows {
+			return product, fmt.Errorf("product::findById %d: no such product", id)
+		}
+
+		return product, fmt.Errorf("product::findById %d: %v", id, err)
+	}
+
+	return product, nil
+}
+
+func CreateDbConnection() *sql.DB {
+	return db.Connect(
+		os.Getenv("USER"),
+		os.Getenv("PASSWD"),
+		os.Getenv("NET_TYPE"),
+		os.Getenv("HOST_PORT"),
+		os.Getenv("DB_NAME"),
+	)
 }

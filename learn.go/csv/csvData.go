a731d5f189d3794b8004d8242e40b912677b593b
dec1i1bel <v.balabanov85@gmail.com>
2025-09-09 11:46:36 +0300

    .

--- learn.go/csv/csvData.go
+++ learn.go/csv/csvData.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"encoding/csv"
+	"fmt"
 	"os"
 )
 
 type Record struct {
-	Name    string
-	Surname string
-	Number  string
+	Name       string
+	Surname    string
+	Number     string
+	LastAccess string
 }
 
 var myData = []Record{}
@@ -37,4 +39,62 @@ func readCSVFile(filepath string) ([][]string, error) {
 	}
 
 	return lines, nil
+}
+
+// запись в CSV-файл
+func saveCSVFile(filepath string) error {
+	csvfile, err := os.Create(filepath)
+
+	if err != nil {
+		return err
+	}
+
+	defer csvfile.Close()
+	csvwriter := csv.NewWriter(csvfile)
+	// изменение разделителя полей по умолчанию на табуляцию
+	csvwriter.Comma = '\t'
+
+	for _, row := range myData {
+		temp := []string{row.Name, row.Surname, row.Number, row.LastAccess}
+		_ = csvwriter.Write(temp)
+		csvwriter.Flush()
+		return nil
+	}
+}
+
+// с помощью readCSVFile() помещает прочитанное в срез myData. Помните, что lines — это срез с двумя измерениями и что каждая строка в lines уже разделена на поля.
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("csvData input output!")
+		return
+	}
+
+	input := os.Args[1]
+	output := os.Args[2]
+	lines, err := readCSVFile(input)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// данные CSV считываются по столбцам — каждая строка представляет собой срез
+	for _, line := range lines {
+		temp := Record{
+			Name:       line[0],
+			Surname:    line[1],
+			Number:     line[2],
+			LastAccess: line[3],
+		}
+
+		myData = append(myData, temp)
+		fmt.Println("temp: ", temp)
+	}
+
+	err = saveCSVFile(output)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }

e0d564488201ab7910c43bf553c3ac0b9667af90
dec1i1bel <v.balabanov85@gmail.com>
2025-09-18 18:49:15 +0300

    .

--- learn.go/phoneBook/phoneBook.go
+++ learn.go/phoneBook/phoneBook.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"math/rand"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -17,6 +19,7 @@ type Entry struct {
 
 var data = []Entry{}
 var index = map[string]int
+var db = "db.csv"
 
 // линейный поиск по срезу data. это медленно, но достаточно при небольшом количестве записей
 func search(key string) *Entry {
@@ -69,10 +72,110 @@ func random(min, max int) int {
 	return rand.Intn(max-min) + min
 }
 
+// получаем доступ ко всему срезу data и помещаем пары индекса и значения среза на карту, используя значение в качестве ключа для карты и индекс среза в качестве значения карты
+func createIndex() error {
+	index = make(map[string]int)
+	for i, k := range data {
+		key := k.Tel
+		index[key] = i
+	}
+
+	return nil
+}
+
+func readCSVFile(filepath string) ([][]string, error) {
+	_, err := os.Stat(filepath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	f, err := os.Open(filepath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer f.Close()
+
+	// CSV-файл читается весь сразу - ReadAll() (построчно - Read())
+	// тип данных lines — [][]string
+	lines, err := csv.NewReader(f).ReadAll()
+
+	if err != nil {
+		return [][]string{}, err
+	}
+
+	return lines, nil
+}
+
+// запись в CSV-файл
+func saveCSVFile(filepath string) error {
+	csvfile, err := os.Create(filepath)
+
+	if err != nil {
+		return err
+	}
+
+	defer csvfile.Close()
+	csvwriter := csv.NewWriter(csvfile)
+	// изменение разделителя полей по умолчанию на табуляцию
+	csvwriter.Comma = '\t'
+
+	for _, row := range data {
+		temp := []string{row.Name, row.Surname, row.Tel, row.LastAccess}
+		_ = csvwriter.Write(temp)
+		csvwriter.Flush()
+	}
+
+	return nil
+}
+
+func deleteEntry(key string) error {
+	// поиск по индексу телефонного номера, чтобы найти место записи в срезе с данными. Если его нет - сообщение об ошибке
+	i, ok := index[key]
+	if !ok {
+		return fmt.Errorf("%s cannot be found!", key)
+	}
+
+	// Если номер телефона найден, то вы удаляете соответствующую запись из среза data
+	// склеивания в новый срез части до элемента и после`
+	data = append(data[:i], data[i+1:]...)
+
+	// обновить индекс (удалить из нег запись)‚ поскольку забота о нем — та цена, которую вы платите за дополнительную скорость, возникающую благодаря ему
+	delete(index, key)
+
+	// сохранить обновленные данные
+	err := saveCSVFile(db)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// toDo: matchTel - аналогично matchInt из regexp/intRE.go. учесть, что возможен +7... или прочие знаки кроме цифр
+func matchTel(tel string) bool {
+
+}
+
+// toDo: это копипаст из другого файла. доработать
+func initS(N, S string, Y int) Entry {
+	if Y < 2000 {
+		return Entry{Name: N, Surname: S, Year: 2000}
+	}
+
+	return Entry{Name: N, Surname: S, Year: Y}
+}
+
+// toDo
+func insert() {
+
+}
+
 // телефонная книга заполняется из csv-файла
 func main() {
 	arguments := os.Args
-	db := "db.csv"
 	if len((arguments)) == 1 {
 		fmt.Println("Usage: insert|delete|search|list <arguments>")
 		return
@@ -111,7 +214,67 @@ func main() {
 	}
 
 	switch arguments[1] {
-	// toDo
+	case "insert":
+		if len(arguments) != 5 {
+			fmt.Println("Usage: insert <Name>, <Surname> <Telephone>")
+			return
+		}
+
+		t := strings.ReplaceAll(arguments[4], "-", "")
+
+		if !matchTel(t) {
+			fmt.Println("Not a valid telephone number:", t)
+			return
+		}
+
+		temp := initS(arguments[2], arguments[3], t)
+
+		if temp != nil {
+			err := insert(temp)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+		}
+	case "delete":
+		if len(arguments) != 3 {
+			fmt.Println("Usage: delete <Number>")
+			return
+		}
+
+		t := strings.ReplaceAll(arguments[2], "-", "")
+		if !matchTel(t) {
+			fmt.Println("Not a valid telephone number:", t)
+			return
+		}
+
+		err := deleteEntry(t)
+		if err != nil {
+			fmt.Println(err)
+		}
+	case "search":
+		if len(arguments) != 3 {
+			fmt.Println("Usage: search <Number>")
+			return
+		}
+
+		t := strings.ReplaceAll(arguments[2], "-", "")
+		if !matchTel(t) {
+			fmt.Println("Not a valid telephone number:", t)
+			return
+		}
+
+		temp := search(t)
+		if temp == nil {
+			fmt.Println("Number not found:", t)
+			return
+		}
+
+		fmt.Println(*temp)
+	case "list":
+		list()
+	default:
+		fmt.Println("Not a valid option")
 	}
 }
 
